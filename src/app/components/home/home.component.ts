03d767959dde8d810793322e18553d7ac61faa9c
maurizz29 <mauriitomas@gmail.com>
2026-07-06 18:54:30 -0300

    fix: corregir logica del widget del clima para considerar la temperatura en Jujuy

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -31,7 +31,7 @@ export class HomeComponent implements OnInit {
     this.http.get(url).subscribe({
       next: (data: any) => {
         this.clima = data.current_weather;
-        this.interpretarClima(this.clima.weathercode);
+        this.interpretarClima(this.clima.weathercode, this.clima.temperature);
         this.cdr.detectChanges(); // Forzamos a Angular a actualizar la pantalla
       },
       error: (err) => {
@@ -43,25 +43,31 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  interpretarClima(code: number) {
-    if (code === 0) {
-      this.iconoClima = 'sunny';
-      this.descripcionClima = '¡Día espectacular para comer al aire libre!';
-    } else if (code >= 1 && code <= 3) {
-      this.iconoClima = 'partly_cloudy_day';
-      this.descripcionClima = 'Lindo día para unas hamburguesas.';
-    } else if (code >= 45 && code <= 48) {
-      this.iconoClima = 'foggy';
-      this.descripcionClima = 'Poca visibilidad, mejor pedir delivery.';
-    } else if ((code >= 51 && code <= 67) || (code >= 80 && code <= 82)) {
+  interpretarClima(code: number, temperature: number) {
+    if ((code >= 51 && code <= 67) || (code >= 80 && code <= 82)) {
       this.iconoClima = 'rainy';
       this.descripcionClima = 'Día lluvioso... ¡ideal para pedir delivery!';
     } else if (code >= 71 && code <= 77) {
       this.iconoClima = 'ac_unit';
-      this.descripcionClima = '¡Hace frío! Pedí desde la cama.';
+      this.descripcionClima = '¡Nieve a la vista! Pedí desde la comodidad de tu casa.';
     } else if (code >= 95 && code <= 99) {
       this.iconoClima = 'thunderstorm';
       this.descripcionClima = '¡Tormenta fuerte! Los deliverys pueden demorar.';
+    } else if (temperature < 15) {
+      this.iconoClima = 'ac_unit';
+      this.descripcionClima = 'Hace un poco de frío. ¡Ideal para pedir algo calentito!';
+    } else if (temperature > 30) {
+      this.iconoClima = 'local_fire_department';
+      this.descripcionClima = '¡Día de mucho calor! Refrescate con nuestras bebidas frías.';
+    } else if (code === 0) {
+      this.iconoClima = 'sunny';
+      this.descripcionClima = '¡Día espectacular para comer al aire libre!';
+    } else if (code >= 1 && code <= 3) {
+      this.iconoClima = 'partly_cloudy_day';
+      this.descripcionClima = 'Lindo día para unas buenas hamburguesas.';
+    } else if (code >= 45 && code <= 48) {
+      this.iconoClima = 'foggy';
+      this.descripcionClima = 'Poca visibilidad, mejor pedir delivery.';
     } else {
       this.iconoClima = 'cloud';
       this.descripcionClima = '¡El clima ideal para una buena comida!';
